06b2bf13255f09d461cbcf3be694ff1862a48458
Khang Hoang <hoanghongkhang@gmail.com>
2025-11-17 08:45:01 +0000

    feat(web): add tailadmin code base

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,27 +1,12 @@
-<script setup>
-import { ref, onMounted, watch } from 'vue'
-import Header from './components/Header.vue'
-import Tabs from './components/Tabs.vue'
-import ServerConsole from './components/ServerConsole.vue'
-
-const activeTab = ref(localStorage.getItem('activeTab') || 'code-editor')
-
-// Save active tab to localStorage when it changes
-watch(activeTab, (newTab) => {
-  localStorage.setItem('activeTab', newTab)
-})
-
-</script>
-
 <template>
-  <div class="app-container flex flex-col h-screen">
-    <Header />
-    <Tabs v-model:activeTab="activeTab" />
-    <div class="content flex-1 overflow-hidden relative bg-darker">
-      <iframe class="w-full h-full border-0" src="/vscode" v-show="activeTab === 'code-editor'"></iframe>
-      <ServerConsole v-show="activeTab === 'console'" />
-    </div>
-  </div>
+  <ThemeProvider>
+    <SidebarProvider>
+      <RouterView />
+    </SidebarProvider>
+  </ThemeProvider>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import ThemeProvider from './components/layout/ThemeProvider.vue'
+import SidebarProvider from './components/layout/SidebarProvider.vue'
+</script>
