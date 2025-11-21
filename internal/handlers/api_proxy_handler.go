55acf5c37090ba214fec816bd5234a9b26f7d430
Khang Hoang <hoanghongkhang@gmail.com>
2025-11-21 18:53:41 +0000

    fix(handlers): correct api proxy url

--- internal/handlers/api_proxy_handler.go
+++ internal/handlers/api_proxy_handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,10 +11,13 @@ import (
 func MCRunnerProxyHandler(apiURLs map[string]string) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		name := c.Params("name")
+		path := c.Params("*")
 		apiURL, ok := apiURLs[strings.ToLower(name)]
 		if !ok {
 			return fiber.NewError(fiber.StatusNotFound)
 		}
-		return proxy.Do(c, apiURL)
+		fullURL := apiURL + "/" + path
+		fmt.Println(fullURL)
+		return proxy.Do(c, fullURL)
 	}
 }
