5b462411e8ebb7a3e5b19a7c488ab3f95570d2f4
Khang Hoang <hoanghongkhang@gmail.com>
2025-11-23 23:25:24 +0700

    feat: clean up redundant code

--- web/src/types/vue3-toastify.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'vue3-toastify' {
-  export const toast: any;
-  const plugin: any;
-  export default plugin;
-  export type ToastContainerOptions = any;
-  export type ToastOptions = any;
-}
